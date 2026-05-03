3e80d80a5d938346b69e8b55fd2f79f68bd753f4
herbatochochlik <dotom555@gmail.com>
2026-05-03 20:47:20 +0200

    CUI: Add folder creation

--- src/console.rs
+++ src/console.rs
@@ -1,4 +1,43 @@
+use crate::filesystem;
+use std::io::stdin;
+
 pub fn init() {
     println!("Initializing Console User Interface!");
     println!("Welcome to RustManager!");
+
+    choose_command();
+}
+
+fn choose_command() {
+    println!(" [0] Add folder \n [1] Add file \n [2] Change directory");
+
+    let input = take_input("What do you want to do?");
+
+    match input.trim() {
+        "0" => console_add_folder(),
+        "1" => println!("Adding file"),
+        "2" => println!("Changing directory"),
+        _ => println!("Invalid choice"),
+    }
+}
+
+fn console_add_folder() {
+    let name = take_input("Name the folder: ");
+
+    filesystem::add_folder("upload", &name);
+    println!("Added folder: {name}");
+
+    choose_command();
+}
+
+fn take_input(msg: &str) -> String {
+    println!("{msg}");
+
+    let mut input: String = String::new();
+
+    stdin().read_line(&mut input).expect("Błąd odczytu");
+
+    let trimmed: &str = input.trim();
+
+    return trimmed.to_string();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::env;
+
 mod console;
+mod filesystem;
 mod graphical;
 
 fn main() {
